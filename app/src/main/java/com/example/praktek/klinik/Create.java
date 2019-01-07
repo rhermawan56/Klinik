2cbd550147ac35644a7e65e50109c4d88fd27560
rizky hermawan <rhermawan56@gmail.com>
2019-01-08 01:24:45 +0700

    Update create, login, db

--- app/src/main/java/com/example/praktek/klinik/Create.java
+++ app/src/main/java/com/example/praktek/klinik/Create.java
@@ -1,8 +1,10 @@
 package com.example.praktek.klinik;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,33 +12,82 @@ import android.widget.Toast;
 
 public class Create extends AppCompatActivity {
 
-    DatabaseHelper db;
-    EditText nama, email, pass, pass2, tlp;
-    Button create, cancel;
+    EditText nama, email, pass, conpass, tlp;
+    private String Snama, Semail, Spass, Sconpass, Stlp;
+    CardView create, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        db = new DatabaseHelper(this);
         nama = (EditText) findViewById(R.id.create_nama);
         email = (EditText) findViewById(R.id.create_email);
-        pass = (EditText) findViewById(R.id.create_password);
-        pass2 = (EditText) findViewById(R.id.create_password2);
+        pass = (EditText) findViewById(R.id.create_pass);
+        conpass = (EditText) findViewById(R.id.create_conpass);
         tlp = (EditText) findViewById(R.id.create_tlp);
-        create = (Button) findViewById(R.id.createbtn_create);
-        cancel = (Button) findViewById(R.id.createbtn_cancel);
+        create = (CardView) findViewById(R.id.create_cardcreate);
+        cancel = (CardView) findViewById(R.id.create_cardcancel);
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Create.this, Login.class);
                 nama.setText("");
                 email.setText("");
+                email.setText("");
                 pass.setText("");
-                pass2.setText("");
+                conpass.setText("");
                 tlp.setText("");
-                create.requestFocus();
+                nama.requestFocus();
+            }
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snama = nama.getText().toString();
+                Semail = email.getText().toString();
+                Spass = pass.getText().toString();
+                Sconpass = conpass.getText().toString();
+                Stlp = tlp.getText().toString();
+                if (Snama.equals("")){
+                    Toast.makeText(Create.this, "nama tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    nama.requestFocus();
+                } else if (Semail.equals("")){
+                    Toast.makeText(Create.this, "email tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    email.requestFocus();
+                } else if (Spass.equals("")){
+                    Toast.makeText(Create.this, "sandi tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    pass.requestFocus();
+                } else if (Sconpass.equals("")){
+                    Toast.makeText(Create.this, "konfirmasi sandi tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    conpass.requestFocus();
+                } else if (Stlp.equals("")){
+                    Toast.makeText(Create.this, "telepon tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                    tlp.requestFocus();
+                } else if (!(Spass.equals(Sconpass))){
+                    Toast.makeText(Create.this, "sandi dan konfirmasi sandi\nharus sama", Toast.LENGTH_SHORT).show();
+                    conpass.setText("");
+                    conpass.requestFocus();
+                } else {
+                    DatabaseAccess db = DatabaseAccess.getInstance(getApplicationContext());
+                    boolean cekEmail = db.cekEmail(Semail);
+                    if (cekEmail == true){
+                        db.CreateAccoount(new ModalLogin(Semail, Spass, Snama, Stlp));
+                        Toast.makeText(Create.this, "akun berhasil didaftarkan", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Create.this, Menu.class);
+                        startActivity(intent);
+                        nama.setText("");
+                        email.setText("");
+                        pass.setText("");
+                        conpass.setText("");
+                        tlp.setText("");
+                        nama.requestFocus();
+                    } else {
+                        Toast.makeText(Create.this, "email sudah terdaftar", Toast.LENGTH_SHORT).show();
+                        email.setText("");
+                        email.requestFocus();
+                    }
+                }
             }
         });
     }
