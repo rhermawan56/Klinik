ad7db0ad0c590057fdf101ec22c51e49a3360fae
rizky hermawan <rhermawan56@gmail.com>
2019-01-08 12:49:17 +0700

    update rumahsakit.java 20%

--- app/src/main/java/com/example/praktek/klinik/RumahSakitActivity.java
+++ app/src/main/java/com/example/praktek/klinik/RumahSakitActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+
 public class RumahSakitActivity extends AppCompatActivity {
+
+    BootstrapButton detailrs1, detailrs2, detailrs3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rumah_sakit);
+        detailrs1 = (BootstrapButton) findViewById(R.id.detail1);
+        detailrs2 = (BootstrapButton) findViewById(R.id.detail2);
+        detailrs3 = (BootstrapButton) findViewById(R.id.detail3);
     }
 
     public void Beranda(View v) {
