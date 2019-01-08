513c3464d92552c70608d5194703b9e49b60f6c8
rizky hermawan <rhermawan56@gmail.com>
2019-01-08 16:24:24 +0700

    update login, menu -> alertdialog, backpress

--- app/src/main/java/com/example/praktek/klinik/Menu.java
+++ app/src/main/java/com/example/praktek/klinik/Menu.java
@@ -1,6 +1,8 @@
 package com.example.praktek.klinik;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -15,6 +17,8 @@ public class Menu extends AppCompatActivity {
 
     TextView name;
     CardView rs, dokter, akun;
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +53,46 @@ public class Menu extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Apakah anda yakin akan logout?");
+        alert
+                .setMessage("Klik Ya untuk logout")
+                .setIcon(R.drawable.app_logo)
+                .setCancelable(false)
+                .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        ModalLogin modalLogin = new ModalLogin();
+                        modalLogin.set_email("");
+                        modalLogin.set_password("");
+                        modalLogin.set_nama("");
+                        modalLogin.set_notlp("");
+                        Menu.this.finish();
+                    }
+                })
+                .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alert.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (backPressedTime + 2000 > System.currentTimeMillis()){
+            backToast.cancel();
+            showDialog();
+            return;
+        } else {
+            backToast = Toast.makeText(Menu.this, "ketuk dua kali untuk keluar", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
--- app/src/main/java/com/example/praktek/klinik/RumahSakitActivity.java
+++ app/src/main/java/com/example/praktek/klinik/RumahSakitActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
