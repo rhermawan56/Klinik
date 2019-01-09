44c723d02dfe74a1fca9c988ce28b645ed853198
inal zainal muttaqien <inal.zainalmuttaqien145@gmail.com>
2019-01-10 00:54:07 +0700

    add detail account

--- app/src/main/java/com/example/praktek/klinik/InfoAkunActivity.java
+++ app/src/main/java/com/example/praktek/klinik/InfoAkunActivity.java
@@ -15,7 +15,7 @@ import java.util.zip.Inflater;
 
 public class InfoAkunActivity extends AppCompatActivity {
 
-    CardView home, logout;
+    CardView home, logout, beranda, rs, dokter;
     TextView nama, email, tlp;
     private View convertView;
     private LayoutInflater inflater;
@@ -25,12 +25,14 @@ public class InfoAkunActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_akun);
-        home = (CardView) findViewById(R.id.info_cardhome);
+//        home = (CardView) findViewById(R.id.info_cardhome);
         nama = (TextView) findViewById(R.id.info_nama);
         email = (TextView) findViewById(R.id.info_email);
         tlp = (TextView) findViewById(R.id.info_tlp);
         logout = (CardView) findViewById(R.id.info_btnlogout);
-
+        beranda = (CardView) findViewById(R.id.dokter_cardberanda);
+        rs = (CardView) findViewById(R.id.dokter_cardrs);
+        dokter = (CardView) findViewById(R.id.dokter_cardrs);
 
 
         inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -54,5 +56,39 @@ public class InfoAkunActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        beranda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoAkunActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+
+        rs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoAkunActivity.this, RumahSakitActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dokter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoAkunActivity.this, BookingDokterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void GoToDetailAccount(View v) {
+        Intent gotodetailaccount = new Intent(InfoAkunActivity.this, DetailAkunActivity.class);
+        startActivity(gotodetailaccount);
+    }
+
+    public void GoToHistoryBooking(View v) {
+        Intent gotohistorybooking = new Intent(InfoAkunActivity.this, HistoryBookingActivity.class);
+        startActivity(gotohistorybooking);
     }
 }
